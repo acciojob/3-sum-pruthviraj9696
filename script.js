3e99128160e11e024060aacb4cea18a0244570ad
pruthviraj9696 <120453319+pruthviraj9696@users.noreply.github.com>
2023-02-19 00:11:59 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function threeSum(arr, target) {
-  //your code here
+function threeSum(arr, x) {
+  let closestSum = Number.MAX_VALUE;
+ 
+    // Run three nested loops each loop
+    // for each element of triplet
+    for(let i = 0; i < arr.length ; i++)
+    {
+        for(let j =i + 1; j < arr.length; j++)
+        {
+            for(let k =j + 1; k < arr.length; k++)
+            {
+                 
+                // Update the closestSum
+                if (Math.abs(x - closestSum) >
+                    Math.abs(x - (arr[i] + arr[j] + arr[k])))
+                    closestSum = (arr[i] + arr[j] + arr[k]);
+            }
+        }
+    }
+     
+    // Return the closest sum found
+    return closestSum;
 }
 
 module.exports = threeSum;
